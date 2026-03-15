60eff8f18849da8df77ba493a16f0c0af3619d18
Alexander Chernikov <chernikov80@gmail.com>
2026-03-15 20:54:05 +0200

    Add exercise for merge intervals in array

---
+++ src/main/java/com/alexchernikov/datastructures/array/merge/intervals/MergeIntervals.java
@@ -0,0 +1,45 @@
+package com.alexchernikov.datastructures.array.merge.intervals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class MergeIntervals {
+    public static void main(String[] args) {
+        int[][] intervals = {
+            {1,3},
+            {2,6},
+            {8,10},
+            {15,18}
+        };
+
+        int[][] result = merge(intervals);
+
+        for (int[] interval : result) {
+            System.out.println(Arrays.toString(interval));
+        }
+    }
+
+    public static int[][] merge(int[][] intervals) {
+        if(intervals.length <= 1) {
+            return intervals;
+        }
+
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        List<int[]> result = new ArrayList<>();
+        int[] current = intervals[0];
+        result.add(current);
+        for (int i = 1; i < intervals.length; i++) {
+            int[] next = intervals[i];
+            if (next[0] <= current[1]) {
+                current[1] = Math.max(current[1], next[1]);
+            } else {
+                current = next;
+                result.add(current);
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
---
+++ src/main/java/com/alexchernikov/datastructures/array/merge/intervals/README.md
@@ -0,0 +1,7 @@
+## Problem definition:
+Merge intervals : Given a collection of intervals, merge all overlapping intervals.
+
+### Example:
+- Input: [[1,3],[2,6],[8,10],[15,18]] 
+- Output: [[1,6],[8,10],[15,18]]
+- Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
